59cfe3790572cee37779465906c8c31b5e237e94
Simon Calvin <simon.calvin@gmail.com>
2017-05-02 09:34:01 -0500

    cleans up Main.elm

--- src/Main.elm
+++ src/Main.elm
@@ -7,16 +7,6 @@ import Dom exposing (..)
 import Task exposing (..)
 
 
-main =
-    program
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
-        }
-
-
-
 -- MODEL
 
 
@@ -31,52 +21,6 @@ type alias Model =
 init : ( Model, Cmd Msg )
 init =
     ( Model "" Nothing Nothing "", attempt (always NoOp) <| focus "tag" )
-
-
-
--- UPDATE
-
-
-type Msg
-    = Input String
-    | Player
-    | Opponent
-    | Copy
-    | NoOp
-
-port copy : String -> Cmd msg
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        Input value ->
-            ( { model | input = value }, Cmd.none )
-
-        Player ->
-            ( { model | input = "", player = Just model.input }, attempt (always NoOp) <| focus "tag" )
-
-        Opponent ->
-            ( { model 
-                | input = ""
-                , opponent = Just model.input
-                , token =  "token" }
-            , Cmd.none 
-            )
-
-        Copy ->
-            ( model, copy model.token )
-
-        NoOp ->
-            ( model, Cmd.none )
-
-
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
 
 
 
@@ -96,22 +40,85 @@ view model =
         case model.player of
             Just player ->
                 div []
-                    [ p [] 
-                        ( [ text <| "Hi, " ++ player ] ++
-                            case model.opponent of
+                    [ p []
+                        ([ text <| "Hi, " ++ player ]
+                            ++ case model.opponent of
                                 Just opponent ->
                                     [ p [] [ text <| "OK. Let's let " ++ opponent ++ " know" ]
-                                    , p [] 
+                                    , p []
                                         [ label [ for "tag" ] [ text <| "Give him/her this:" ]
                                         , input [ type_ "text", id "tag", value model.token, onInput Input ] []
-                                        , input [ type_ "button", value "📋", onClick Copy] []
+                                        , input [ type_ "button", value "📋", onClick Copy ] []
                                         ]
                                     ]
 
                                 Nothing ->
-                                    [ form "Who do you want to flip?" "OK" Opponent ] 
+                                    [ form "Who do you want to flip?" "OK" Opponent ]
                         )
                     ]
 
             Nothing ->
                 form "What's your name?" "OK" Player
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Input String
+    | Player
+    | Opponent
+    | Copy
+    | NoOp
+
+
+port copy : String -> Cmd msg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Input value ->
+            ( { model | input = value }, Cmd.none )
+
+        Player ->
+            ( { model | input = "", player = Just model.input }, attempt (always NoOp) <| focus "tag" )
+
+        Opponent ->
+            ( { model
+                | input = ""
+                , opponent = Just model.input
+                , token = "token"
+              }
+            , Cmd.none
+            )
+
+        Copy ->
+            ( model, copy model.token )
+
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- MAIN
+
+
+main : Program Never Model Msg
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
